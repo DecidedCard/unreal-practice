5a59a68252ded3e15236ef15ef3e4429e740758a
heajun <heajun35@gmail.com>
2025-12-31 20:18:41 +0900

    Feat: 문자열을 자르고 검색하는 기능

--- Source/MyProject/MyGameInstance.cpp
+++ Source/MyProject/MyGameInstance.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "MyGameInstance.h"
@@ -7,10 +7,29 @@ void UMyGameInstance::Init()
 {
 	Super::Init();
 
-	TCHAR LogCharArray[] = TEXT("Hello Unreal");
+	TCHAR LogCharArray[] = TEXT("Hello Unreal");  // TCHAR 배열 선언 및 초기화
 	UE_LOG(LogTemp, Log, TEXT("%s"), LogCharArray);
 
-	FString LogCharString = LogCharArray;
+	FString LogCharString = LogCharArray;  // TCHAR 배열을 FString으로 변환
 	UE_LOG(LogTemp, Log, TEXT("%s"), *LogCharString);
+
+	const TCHAR* LongCharPtr = *LogCharString;  // 포인터 연산자를 사용해서 FString에서 TCHAR*로 변환
+	TCHAR* LogCharDataPtr = LogCharString.GetCharArray().GetData();  // GetCharArray()와 GetData()를 사용해서 TCHAR*로 변환
+
+	TCHAR LogCharArrayWithSize[100];
+	FCString::Strcpy(LogCharArrayWithSize, LogCharString.Len(), *LogCharString); // FCString::Strcpy 사용, 저수준의 복사
+
+	if (LogCharString.Contains(TEXT("unreal"), ESearchCase::IgnoreCase))  // 대소문자 무시하고 포함 여부
+	{
+		int32 Index = LogCharString.Find(TEXT("unreal"), ESearchCase::IgnoreCase);  // 대소문자 무시하고 위치 찾기
+		FString EndString = LogCharString.Mid(Index);  // Mid() 함수로 부분 문자열 추출
+		UE_LOG(LogTemp, Log, TEXT("Find Test: %s"), *EndString);  // 결과 출력
+	}
+
+	FString Left, Right; // 분할된 문자열을 저장할 변수
+	if (LogCharString.Split(TEXT(" "), &Left, &Right)) // Split() 함수로 문자열 분할
+	{
+		UE_LOG(LogTemp, Log, TEXT("Split Test: %s 와 %s"), *Left, *Right);
+	}
 }
  
