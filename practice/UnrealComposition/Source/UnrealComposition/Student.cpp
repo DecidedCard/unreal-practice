d61f22cfd46994202fb1a8b6a7096053bfc9678c
heajun <heajun35@gmail.com>
2026-01-03 03:37:51 +0900

    Feat: 8강 언리얼 C++ 설계2 - 컴포지션

---
+++ practice/UnrealComposition/Source/UnrealComposition/Student.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Student.h"
+#include "Card.h"
+
+UStudent::UStudent()
+{
+	Name = TEXT("학생");
+	Card->SetCardType(ECardType::Student);
+}
+
+void UStudent::DoLesson()
+{
+	ILessonInterface::DoLesson();
+	UE_LOG(LogTemp, Log, TEXT("%s 이(가) 수업을 듣고 있습니다."), *Name);
+}
---
+++ practice/UnrealComposition/Source/UnrealComposition/Teacher.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Teacher.h"
+#include "Card.h"
+
+UTeacher::UTeacher()
+{
+	Name = TEXT("선생님");
+	Card->SetCardType(ECardType::Teacher);
+
+}
+
+void UTeacher::DoLesson()
+{
+	ILessonInterface::DoLesson();
+	UE_LOG(LogTemp, Log, TEXT("%s 이(가) 수업을 가르치고 있습니다."), *Name);
+}
